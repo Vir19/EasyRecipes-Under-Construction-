c1d8b619e6975f5e504ea52bf6c7bfafd684bac5
Vir19 <viir19@hotmail.com>
2024-04-04 21:09:12 +0200

    header mobile in progress

--- React/src/components/Header.jsx
+++ React/src/components/Header.jsx
@@ -1,4 +1,3 @@
-//import PropTypes from 'prop-types';
 import { NavLink } from "react-router-dom";
 import logo from "../images/EasyRecipesremovebg.png";
 
@@ -6,26 +5,30 @@ function Header() {
   return (
     <header className="header">
       <img src={logo} className="logo_er" alt="LogoEasyRecipes" />
-      <h1 className="slogan">... because life is already hard enough</h1>
-      <nav className="navBar">
-        <NavLink className="linkNav" to="#">
-          Home
-        </NavLink>
-        <NavLink className="linkNav" to="#">
-          Login
-        </NavLink>
-        <NavLink className="linkNav" to="#">
-          Sign in
-        </NavLink>
-        <NavLink className="linkNav" to="#">
-          Close session
-        </NavLink>
-      </nav>
+      <h1 className="slogan hiddenSlogan">
+        because life is already hard enough
+      </h1>
+      <div className="navigationDiv">
+        <div className="headerSquare hs-hidden">
+          <i className="fa-solid fa-burger iconburger"></i>
+        </div>
+        <nav className="navBar navHid">
+          <NavLink className="linkNav" to="#">
+            Home
+          </NavLink>
+          <NavLink className="linkNav" to="#">
+            Login
+          </NavLink>
+          <NavLink className="linkNav" to="#">
+            Sign in
+          </NavLink>
+          <NavLink className="linkNav" to="#">
+            Log out
+          </NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
-
-/* Header.propTypes = {
-}; */
 
 export default Header;
