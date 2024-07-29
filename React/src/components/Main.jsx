d11b6b5955cff6c0b1c9e6defe2d0b90481b1b7e
Vir19 <viir19@hotmail.com>
2024-07-29 20:24:33 +0200

    bocetando

--- React/src/components/Main.jsx
+++ React/src/components/Main.jsx
@@ -2,23 +2,38 @@
 
 function Main() {
   return (
-    <main className="name">
-      <img
-        src="https://i.blogs.es/8c3360/pollo_curry/1366_2000.jpg"
-        alt="Pollo al Curry"
-      />
-      {/* la lista ingredientes seguramente será un comp */}
-      <section>
-        <ul>
-          <li>Pechuga de pollo</li>
-          <li>Cebolleta</li>
-          <li>Dientes de ajo</li>
-          <li>Jengibre fresco</li>
-          <li>Guindilla pequeña</li>
-        </ul>
-      </section>
-      <section>
-        <p></p>
+    <main className="main">
+      <div className="flexMain">
+        <img
+          className="foodPic"
+          src="https://i.blogs.es/8c3360/pollo_curry/1366_2000.jpg"
+          alt="Pollo al Curry"
+        />
+        {/* la lista ingredientes seguramente será un comp */}
+        <section className="ingredientList">
+          <ul>
+            <li>Pechuga de pollo</li>
+            <li>Cebolleta</li>
+            <li>Dientes de ajo</li>
+            <li>Jengibre fresco</li>
+            <li>Guindilla pequeña</li>
+          </ul>
+        </section>
+      </div>
+      <section className="preparation">
+        <p>
+          Lo primero es tener preparados todos los ingredientes. Cortar el pollo
+          en piezas de un bocado. Picar la cebolleta y muy fino el diente de
+          ajo, el jengibre y la guindilla sin las semillas, si la usamos. Si
+          usamos cebolla troceada congelada ahorraremos aún más tiempo. Calentar
+          un poco de aceite en una sartén o cazuela y freír la cebolleta 2
+          minutos. Agregar el ajo, el jengibre y la guindilla, dar unas vueltas
+          e incorporar el tomate. Pasado 1 minuto, agregar todas las especias y
+          remover bien. Incorporar el pollo, salpimentar y mezclar. Cocinar a
+          fuego fuerte unos pocos minutos, hasta que esté dorado por todas
+          partes. Cubrir con la leche de coco o nata y dejar reducir
+          ligeramente. Servir con perejil o cilantro fresco picado.{" "}
+        </p>
       </section>
     </main>
   );
